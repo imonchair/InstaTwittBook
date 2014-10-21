ab22f8549eb947c2fd40c5bcc87c2e3c2fe68d5a
jack <jack.gifford2264@gmail.com>
2014-10-21 20:35:35 +1100

    Wrote a login script, it's about as basic as your friend on tumblr

--- hello.py
+++ hello.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
+
+# use decorators to link the function to a url
 @app.route('/')
-def hello_world():
-    return render_template(login.html)
+def home():
+    return render_template('index.html')  # return a string
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
 
 if __name__ == '__main__':
     app.run()
